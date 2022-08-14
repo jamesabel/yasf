149531b2752e8d594b8de730c1e732c64e1ac1cc
James Abel <j@abel.co>
2022-08-14 14:35:38 -0700

    release

--- scripts/pypi.bat
+++ scripts/pypi.bat
@@ -3,6 +3,7 @@ cd ..
 del /Q yasf.egg-info\*.*
 del /Q build\*.*
 del /Q dist\*.*
+copy README.md yasf
 copy /Y LICENSE LICENSE.txt
 venv\Scripts\python.exe setup.py bdist_wheel
 venv\Scripts\twine upload dist/*
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 from yasf.__version__ import __version__, __title__, __author__, __author_email__, __url__, __download_url__, __description__
 
-readme_file_path = os.path.join(__title__, "readme.rst")
+readme_file_path = os.path.join(__title__, "readme.md")
 
 with open(readme_file_path, encoding="utf-8") as f:
     long_description = "\n" + f.read()
@@ -13,7 +13,7 @@ setup(
     name=__title__,
     description=__description__,
     long_description=long_description,
-    long_description_content_type="text/md",
+    long_description_content_type="text/markdown",
     version=__version__,
     author=__author__,
     author_email=__author_email__,
